e8ec8aa9bf27585db1c23a8ed70984bed5cee78a
Hillary Chukwuma Prince <princehillary813@gmail.com>
2026-01-11 01:19:58 +0100

    add updated CRUD task

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+# Custom admin class
+class BookAdmin(admin.ModelAdmin):
+    # Columns to display in the list view
+    list_display = ('title', 'author', 'publication_year')
+    
+    # Enable filters in the sidebar
+    list_filter = ('author', 'publication_year')
+    
+    # Add search functionality
+    search_fields = ('title', 'author')
+
+# Register Book model with custom admin
+admin.site.register(Book, BookAdmin)
