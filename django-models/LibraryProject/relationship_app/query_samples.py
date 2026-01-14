f04f6113058c92ad39c67d9db64cd1073bfd05ed
Hillary Chukwuma Prince <princehillary813@gmail.com>
2026-01-14 17:46:36 +0100

    replaced library.library to librarian.objects.get(library=library)

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -13,9 +13,10 @@ def get_books_in_library(library_name):
 
 def get_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian
+    return Librarian.objects.get(library=library)
 
 
 def get_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
     return Book.objects.filter(author=author)
+
