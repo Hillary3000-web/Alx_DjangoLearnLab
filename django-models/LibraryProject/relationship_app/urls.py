4c89d9dd3ac44ce73c36d708598bc2c39b8ae258
Hillary Chukwuma Prince <princehillary813@gmail.com>
2026-01-16 16:21:48 +0100

    updated urls.py to meet requirement

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,6 +3,14 @@ from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
+    path('books/', views.book_list, name='book_list'),
+    path('books/add_book/', views.add_book, name='add_book'),          # ✅ must be "add_book/"
+    path('books/<int:pk>/edit_book/', views.edit_book, name='edit_book'),  # ✅ must be "edit_book/"
+    path('books/<int:pk>/delete_book/', views.delete_book, name='delete_book'), # ✅ must be "delete_book/"
+]
+
+
+urlpatterns = [
     path(
         'login/',
         LoginView.as_view(template_name='relationship_app/login.html'),
