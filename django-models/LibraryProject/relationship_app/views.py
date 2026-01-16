28ffb87d024a4afae9d18eb088c34abfa7eaf69a
Hillary Chukwuma Prince <princehillary813@gmail.com>
2026-01-16 15:39:36 +0100

    Implement user authentication with login, logout, and registration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,16 +1,27 @@
-from django.shortcuts import render
-from django.views.generic.detail import DetailView
-from .models import Book
-from .models import Library
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView
+from  django.contrib.auth.views import LogoutView
 
-# Function-based view to list all books
-def list_books(request):
-    books = Book.objects.all()
-    context = {'books': books}
-    return render(request, 'relationship_app/list_books.html', context)
+# Login view using Django built-in LoginView
+class UserLoginView(LoginView):
+    template_name = 'relationship_app/login.html'
 
-# Class-based view to display library details
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+# Logout view using Django built-in LogoutView
+class UserLogoutView(LogoutView):
+    template_name = 'relationship_app/logout.html'
+    http_method_names = ['get']
+
+# Registration view
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+
+    return render(request, 'relationship_app/register.html', {'form': form})
