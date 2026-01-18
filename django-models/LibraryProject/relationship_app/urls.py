5e3f3cd5980288218bb827b3e79556e261f5f939
Hillary Chukwuma Prince <princehillary813@gmail.com>
2026-01-18 12:48:06 +0100

    corrected version of task 3

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -41,3 +41,11 @@ urlpatterns += [
     path('list_books/', list_books, name='list_books'),
     path('library_detail/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
+
+from .views import admin_view, librarian_view, member_view
+
+urlpatterns += [
+    path('admin_view/', admin_view, name='admin_view'),
+    path('librarian_view/', librarian_view, name='librarian_view'),
+    path('member_view/', member_view, name='member_view'),
+]
