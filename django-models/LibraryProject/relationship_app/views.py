3c0e13d2e67ffa2dba98e3cbaf96b6220494e942
Hillary Chukwuma Prince <princehillary813@gmail.com>
2026-01-18 12:33:51 +0100

    mistake corrected

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -107,3 +107,15 @@ def delete_book(request, pk):
         book.delete()
         return redirect('book_list')
     return render(request, 'relationship_app/book_confirm_delete.html', {'book': book})
+
+from django.views.generic.detail import DetailView
+from .models import Library
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
