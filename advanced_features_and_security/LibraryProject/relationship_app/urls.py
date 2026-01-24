75574e79ad803a924dabac322c5d85ebd80ffdd0
Hillary Chukwuma Prince <princehillary813@gmail.com>
2026-01-24 12:24:03 +0100

    Fix bookshelf models and finalize CustomUser migration

---
+++ advanced_features_and_security/LibraryProject/relationship_app/urls.py
@@ -0,0 +1,59 @@
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+
+urlpatterns = [
+    path('books/', views.book_list, name='book_list'),
+    path('books/add_book/', views.add_book, name='add_book'),          # ✅ must be "add_book/"
+    path('books/<int:pk>/edit_book/', views.edit_book, name='edit_book'),  # ✅ must be "edit_book/"
+    path('books/<int:pk>/delete_book/', views.delete_book, name='delete_book'), # ✅ must be "delete_book/"
+]
+
+
+urlpatterns = [
+    path(
+        'login/',
+        LoginView.as_view(template_name='relationship_app/login.html'),
+        name='login'
+    ),
+    path(
+        'logout/',
+        LogoutView.as_view(template_name='relationship_app/logout.html'),
+        name='logout'
+    ),
+    path(
+        'register/',
+        views.register,
+        name='register'
+    ),
+]
+
+urlpatterns = [
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
+]
+
+# Appending new views and patterns to the existing list
+from .views import list_books, LibraryDetailView
+
+urlpatterns += [
+    path('list_books/', list_books, name='list_books'),
+    path('library_detail/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+]
+
+from .views import admin_view, librarian_view, member_view
+
+urlpatterns += [
+    path('admin_view/', admin_view, name='admin_view'),
+    path('librarian_view/', librarian_view, name='librarian_view'),
+    path('member_view/', member_view, name='member_view'),
+]
+
+from .views import add_book, edit_book, delete_book
+
+urlpatterns += [
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:pk>/', edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', delete_book, name='delete_book'),
+]
