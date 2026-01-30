87fd728d0e68367a5f377b521d020517ae1e69cb
Hillary Chukwuma Prince <princehillary813@gmail.com>
2026-01-30 20:03:52 +0100

    feat: implement CRUD operations using BookViewSet and DefaultRouter

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
-from rest_framework import generics
-from .models import Book  
+from rest_framework import viewsets 
+from .models import Book
 from .serializers import BookSerializer
+from rest_framework import generics
 
-# Create your views here.
-class BookList(generics.ListAPIView): 
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
