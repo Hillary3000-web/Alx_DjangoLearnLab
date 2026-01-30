66f68a84a5f76f14178e198fba3e9ff6d71471a8
Hillary Chukwuma Prince <princehillary813@gmail.com>
2026-01-30 20:17:34 +0100

    feat: add token authentication and IsAuthenticated permissions

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets 
+from rest_framework import viewsets, permissions
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics
@@ -7,7 +7,5 @@ from rest_framework import generics
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
-class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
