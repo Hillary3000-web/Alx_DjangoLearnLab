0026b789a3d85a93b9ce9f1d1f4ecd63d7c6f6b1
Hillary Chukwuma Prince <princehillary813@gmail.com>
2026-02-07 19:19:22 +0100

    feat: implement advanced query features and add project documentation

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,15 +2,26 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
+from django_filters import rest_framework as filters 
+from rest_framework import filters as drf_filters 
 
 class BookListView(generics.ListAPIView):
-    """Retrieves the list of all books. Accessible to everyone."""
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly] # Allows public read
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    filter_backends = [
+        filters.DjangoFilterBackend,
+        drf_filters.SearchFilter,    
+        drf_filters.OrderingFilter 
+    ]
+
+    filterset_fields = ['title', 'author', 'publication_year']
+    search_fields = ['title', 'author__name']
+    ordering_fields = ['title', 'publication_year']
+    ordering = ['title']
 
 class BookDetailView(generics.RetrieveAPIView):
-    """Retrieves a single book by ID. Accessible to everyone."""
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
@@ -33,4 +44,5 @@ class BookDeleteView(generics.DestroyAPIView):
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
 
-    
+
+
