32d4c1102d5257033a32e5299b0ac0fce46addde
Hillary Chukwuma Prince <princehillary813@gmail.com>
2026-02-13 03:14:11 +0100

    chore: Secure settings and stage final project files

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v3!v=f6i#_vokw2=q4opl9(p1r+35$ewg1s9q=e9-!af^nh2_k'
+SECRET_KEY = 'django-insecure-v3!v=f6i#_vokw2=q4opl9(p1r+35$ewg1s9q=e9-!af^nh2_p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
