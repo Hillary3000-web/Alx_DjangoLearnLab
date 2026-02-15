3131023c645ca2a121946e5da7884e183fffe1d8
Hillary Chukwuma Prince <princehillary813@gmail.com>
2026-02-15 18:23:17 +0100

    feat: add blog post CRUD views and permissions

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -20,3 +20,60 @@ def profile(request):
 
 def home(request):
     return render(request, 'blog/index.html')
+
+from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView, 
+    UpdateView, 
+    DeleteView
+)
+from .models import Post
+
+# 1. List View: Accessible to everyone
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-published_date']
+
+# 2. Detail View: Accessible to everyone
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+# 3. Create View: Requires Login
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # Set logged-in user as author
+        return super().form_valid(form)
+
+# 4. Update View: Requires Login + Must be Author
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+# 5. Delete View: Requires Login + Must be Author
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = '/'  # Redirect to list view after deleting
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
